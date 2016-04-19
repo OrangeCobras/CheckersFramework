b4f90333fa05f0bd3c4e3c3ac5df08b07519a41d
Hendrik Werner <hendrik.to@gmail.com>
2016-04-19 23:52:27 +0200

    Add a class to represent points in two dimensions.

---
+++ Point.java
@@ -0,0 +1,31 @@
+package checkers;
+
+/**
+ *
+ * @author Hendrik
+ */
+public class Point {
+
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return the x coordinate
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return the y coordinate
+     */
+    public int getY() {
+        return y;
+    }
+
+}
