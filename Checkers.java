fa4dc9b4b83cb63d81e43e73d711d82daf2bfd44
Hendrik Werner <hendrik.to@gmail.com>
2016-04-19 18:09:51 +0200

    Initial commit

---
+++ Checkers.java
@@ -0,0 +1,16 @@
+package checkers;
+
+/**
+ *
+ * @author Hendrik
+ */
+public class Checkers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
