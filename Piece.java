343a6d655c3d31e8afb5276f1a09eb5fb5864c01
Hendrik Werner <hendrik.to@gmail.com>
2016-04-19 22:45:31 +0200

    Add an enumeration of pieces.

---
+++ Piece.java
@@ -0,0 +1,32 @@
+package checkers;
+
+/**
+ *
+ * @author Hendrik
+ */
+public enum Piece {
+    Black(Color.Black, false),
+    BlackKing(Color.Black, true),
+    White(Color.White, false),
+    WhiteKing(Color.White, true);
+
+    private final Color color;
+    private final boolean king;
+
+    /**
+     * @param color the color of the piece
+     * @param isKing whether the piece is a king
+     */
+    private Piece(Color color, boolean isKing) {
+        this.color = color;
+        this.king = isKing;
+    }
+
+    /**
+     * @return whether this piece is a king
+     */
+    public boolean isKing() {
+        return king;
+    }
+
+}
