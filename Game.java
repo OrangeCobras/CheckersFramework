8c4ba4b31972dc6b62828c1fb0d4d605ed2a7836
Hendrik Werner <hendrik.to@gmail.com>
2016-04-20 22:37:27 +0200

    Add a class to represent a Checkers game.

---
+++ Game.java
@@ -0,0 +1,41 @@
+package CheckersFramework;
+
+import java.util.LinkedList;
+
+/**
+ * A Checkers game with international rules.
+ *
+ * @author Hendrik
+ */
+public class Game {
+
+    private final Board board;
+    private final MoveGetter input;
+    private final LinkedList<View> views;
+    private Status status;
+
+    public Game(MoveGetter input) {
+        this.board = new Board(10);
+        this.input = input;
+        this.views = new LinkedList<>();
+        this.status = Status.TurnWhitePlayer;
+    }
+
+    public void start() {
+        /***** TODO *****/
+        // manage game status
+        // get moves
+        // validate moves
+        // execute moves
+        // update views
+    }
+
+    private void updateViews() {
+        views.stream().forEach((v) -> {
+            v.setBoard(board);
+            v.setStatus(status);
+            v.invalidate();
+        });
+    }
+
+}
