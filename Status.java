c3659fa597d2ce7631e0a3497a7fe47f390b25da
Hendrik Werner <hendrik.to@gmail.com>
2016-04-27 19:49:13 +0200

    Define and implement a method to invert a status.

--- Status.java
+++ Status.java
@@ -5,8 +5,30 @@ package CheckersFramework;
  * @author Hendrik
  */
 public enum Status {
-    TurnWhitePlayer,
-    TurnBlackPlayer,
-    GameEnded;
+    TurnWhitePlayer {
+        @Override
+        public Status invert() {
+            return TurnBlackPlayer;
+        }
+    },
+    TurnBlackPlayer {
+        @Override
+        public Status invert() {
+            return TurnWhitePlayer;
+        }
+    },
+    GameEnded {
+        @Override
+        public Status invert() {
+            return this;
+        }
+    };
+
+    /**
+     * Invert this status.
+     *
+     * @return the inverse of this
+     */
+    public abstract Status invert();
 
 }
